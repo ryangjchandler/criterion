14e155408d5529e99d1027e5f1910830741e0c6a
Ryan Chandler <ryangjchandler@gmail.com>
2024-11-26 20:10:26 +0000

    wip

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sleep-5', function () {
+    sleep(5);
+
+    return 'Sorry, I slept for 5 seconds!';
+});
